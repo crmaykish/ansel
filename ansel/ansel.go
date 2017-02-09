99532d1f584f787f9f9e3a8a50955c9284b672e0
crmaykish <crmaykish@gmail.com>
2017-02-08 20:18:55 -0500

    Reading sensor data over serial

--- ansel/ansel.go
+++ ansel/ansel.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tarm/serial"
+
+	"bufio"
+	"log"
 )
 
 func main() {
-	fmt.Printf("Starting Ansel control program...")
+	fmt.Printf("Starting Ansel control program...\n")
+
+	c := &serial.Config{Name: "/dev/ttyUSB1", Baud: 9600}
+	s, err := serial.OpenPort(c)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(s)
+
+	for {
+		reply, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf(reply)
+	}
+
 }
