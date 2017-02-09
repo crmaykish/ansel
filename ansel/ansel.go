b4462834cfd67d3e0be6b818b1583c5bfb999feb
crmaykish <crmaykish@gmail.com>
2017-02-08 21:18:38 -0500

    Parsing sensor data to ints

--- ansel/ansel.go
+++ ansel/ansel.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/tarm/serial"
 
 	"bufio"
 	"log"
+	"strings"
 )
+
+var sensorData map[int]int
 
 func main() {
 	fmt.Printf("Starting Ansel control program...\n")
 
+	sensorData = make(map[int]int)
+
+	// TODO: add read timeout
 	c := &serial.Config{Name: "/dev/ttyUSB1", Baud: 9600}
 	s, err := serial.OpenPort(c)
 
@@ -22,12 +29,27 @@ func main() {
 	reader := bufio.NewReader(s)
 
 	for {
-		reply, err := reader.ReadString('\n')
+		reading, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf(reply)
+		p := strings.Split(strings.TrimSpace(reading), ":")
+
+		k, err := strconv.Atoi(p[0])
+		if err != nil {
+			fmt.Printf("Error parsing key: %v\n", k)
+		}
+
+		v, err := strconv.Atoi(p[1])
+		if err != nil {
+			fmt.Printf("Error parsing value: %v\n", v)
+		}
+
+		sensorData[k] = v
+
+		fmt.Printf("%d : %d\n", k, sensorData[k])
+
 	}
 
 }
